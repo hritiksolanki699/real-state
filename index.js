c5704a1bdd19a9e35e69dcd03a67384f4df7f8c9
Hritik Solanki <hritiksolanki699@gmail.com>
2023-07-22 19:38:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import propertyRouter from "./routes/property.routes.js";
 
 dotenv.config();
 
-const app = express();
+const app = express();  
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
